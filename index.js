af922f25b31210b11d60e6628081d16f165f4129
Ash Reid <ashley.reid013@gmail.com>
2023-04-30 00:36:08 -0600

    adds starter tests as well as starter classes

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Runs the app using imports from lib/
+
+// TODO: Import necessary files from lib/
+// TODO: require inquirer
+// TODO: require fs
+
+// TODO: Use inquirer to send prompts and accept user inputs
+
+// TODO: User can enter up to only 3 chars when prompted for text
+
+// TODO: Text color prompt accepts a color keyword or hexidecimal number
+
+// TODO: Provide a list to the user to choose a shape: circle, triangle, square
+
+// TODO: Shape color prompt accepts a color keyword or hexidecimal number
+
+// TODO: After prompts, create svg file called 'logo.svg'
+
+// TODO: Output text to cli reads, "Generated logo.svg"
+
