e82eb0dd2f921aec9b16d43b83ab23f5fc9d9f3d
Ash Reid <ashley.reid013@gmail.com>
2023-05-03 10:09:50 -0600

    working files for generating logos

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
 // Runs the app using imports from lib/
+const Queries = require('./lib/queries');
 
-// TODO: Import necessary files from lib/
-// TODO: require inquirer
-// TODO: require fs
+const queries = new Queries();
 
-// TODO: Use inquirer to send prompts and accept user inputs
-
-// TODO: User can enter up to only 3 chars when prompted for text
-
-// TODO: Text color prompt accepts a color keyword or hexidecimal number
-
-// TODO: Provide a list to the user to choose a shape: circle, triangle, square
-
-// TODO: Shape color prompt accepts a color keyword or hexidecimal number
-
-// TODO: After prompts, create svg file called 'logo.svg'
-
-// TODO: Output text to cli reads, "Generated logo.svg"
-
+queries.run();
---
+++ tests/shapes.test.js
@@ -0,0 +1,112 @@
+// // Jest tests for shapes
+// // example: UofU/01-Activities/28-Stu_Mini-Project/Main/tests/taskListItem.test.js
+
+// // Example test
+// // const shape = new Triangle();
+
+// // shape.setColor('blue');
+// // expect(shape.render()).toEqual(
+// //     '<polygon points="150, 18 244, 182 56, 182" fill="blue"/>'
+// // );
+
+// const Shapes = require('../lib/shapes');
+
+// describe('Shapes', () => {
+//     // TODO: Test triangle class for a `render()` method that returns a string for the corresponding SVG file with the given shape color.
+//     test('should render the correct TRIANGLE SVG with given color', () => {
+//         // Not sure if this is the right way to do it or not
+//         const shape = new Shapes([triangle]);
+//         // TODO: update expectedSVG
+//         const expectedSVG = '<polygon points="150, 18 244, 182 56, 182" fill="green"/>';
+//         shape.setColor('green');
+
+//         expect(shape.render()).toEqual(expectedSVG);
+//     });
+    
+//     // TODO: Test circle class for a `render()` method that returns a string for the corresponding SVG file with the given shape color.
+//     test('should render the correct CIRCLE SVG with given color', () => {
+//         // Not sure if this is the right way to do it or not
+//         const shape = new Shapes([circle]);
+//         // TODO: update expectedSVG
+//         const expectedSVG = '<polygon points="150, 18 244, 182 56, 182" fill="blue"/>';
+//         shape.setColor('blue');
+
+//         expect(shape.render()).toEqual(expectedSVG);
+//     });
+
+//     // TODO: Test square class for a `render()` method that returns a string for the corresponding SVG file with the given shape color.
+//     test('should render the correct SQUARE SVG with given color', () => {
+//         // Not sure if this is the right way to do it or not
+//         const shape = new Shapes([square]);
+//         // TODO: update expectedSVG
+//         const expectedSVG = '<polygon points="150, 18 244, 182 56, 182" fill="violet"/>';
+//         shape.setColor('violet');
+
+//         expect(shape.render()).toEqual(expectedSVG);
+//     });
+
+//     test('should only have 3 letters', () => {
+//         // TODO: add test to make sure only 3 letters can be accepted
+//     });
+// });
+
+// Import child classes
+const { Triangle, Circle, Square } = require('./lib/shapes');
+
+// Triangle tests
+describe('Triangle', () => {
+  const triangle = new Triangle('triangle', 'red');
+
+  test('has type property', () => {
+    expect(triangle.type).toBe('triangle');
+  });
+
+  test('has color property', () => {
+    expect(triangle.color).toBe('red');
+  });
+
+  test('renders a triangle SVG element', () => {
+    expect(triangle.render()).toBe('<polygon points="150, 0 300, 200 0, 200" fill="red" />');
+  });
+});
+
+// Circle tests
+describe('Circle', () => {
+  const circle = new Circle('circle', 'blue');
+
+  test('has type property', () => {
+    expect(circle.type).toBe('circle');
+  });
+
+  test('has color property', () => {
+    expect(circle.color).toBe('blue');
+  });
+
+  test('renders a circle SVG element', () => {
+    expect(circle.render()).toBe('<circle cx="150" cy="100" r="100" fill="blue" />');
+  });
+});
+
+// Square tests
+describe('Square', () => {
+  const square = new Square('square', 'green');
+
+  test('has type property', () => {
+    expect(square.type).toBe('square');
+  });
+
+  test('has color property', () => {
+    expect(square.color).toBe('green');
+  });
+
+  test('renders a square SVG element', () => {
+    expect(square.render()).toBe('<rect width="100%" height="100%" fill="green" />');
+  });
+});
+
+
+
+
+
+
+
