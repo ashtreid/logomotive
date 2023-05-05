c3ca5ea2e6dae842cacc0dca184426635869bb23
Ash Reid <ashley.reid013@gmail.com>
2023-05-04 21:27:19 -0600

    adds tests

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -1,119 +1,92 @@
-// // Jest tests for shapes
-// // example: UofU/01-Activities/28-Stu_Mini-Project/Main/tests/taskListItem.test.js
-
-// // Example test
-// // const shape = new Triangle();
-
-// // shape.setColor('blue');
-// // expect(shape.render()).toEqual(
-// //     '<polygon points="150, 18 244, 182 56, 182" fill="blue"/>'
-// // );
-
-// const Shapes = require('../lib/shapes');
-
-// describe('Shapes', () => {
-//     // TODO: Test triangle class for a `render()` method that returns a string for the corresponding SVG file with the given shape color.
-//     test('should render the correct TRIANGLE SVG with given color', () => {
-//         // Not sure if this is the right way to do it or not
-//         const shape = new Shapes([triangle]);
-//         // TODO: update expectedSVG
-//         const expectedSVG = '<polygon points="150, 18 244, 182 56, 182" fill="green"/>';
-//         shape.setColor('green');
-
-//         expect(shape.render()).toEqual(expectedSVG);
-//     });
-    
-//     // TODO: Test circle class for a `render()` method that returns a string for the corresponding SVG file with the given shape color.
-//     test('should render the correct CIRCLE SVG with given color', () => {
-//         // Not sure if this is the right way to do it or not
-//         const shape = new Shapes([circle]);
-//         // TODO: update expectedSVG
-//         const expectedSVG = '<polygon points="150, 18 244, 182 56, 182" fill="blue"/>';
-//         shape.setColor('blue');
-
-//         expect(shape.render()).toEqual(expectedSVG);
-//     });
-
-//     // TODO: Test square class for a `render()` method that returns a string for the corresponding SVG file with the given shape color.
-//     test('should render the correct SQUARE SVG with given color', () => {
-//         // Not sure if this is the right way to do it or not
-//         const shape = new Shapes([square]);
-//         // TODO: update expectedSVG
-//         const expectedSVG = '<polygon points="150, 18 244, 182 56, 182" fill="violet"/>';
-//         shape.setColor('violet');
-
-//         expect(shape.render()).toEqual(expectedSVG);
-//     });
-
-//     test('should only have 3 letters', () => {
-//         // TODO: add test to make sure only 3 letters can be accepted
-//     });
-// });
-
-// Import child classes
+const { TextSVG } = require('../lib/text');
 const { Triangle, Circle, Square } = require('../lib/shapes');
 
 const keywordColors = ['blue', 'violet', 'pink', 'red', 'black', 'green', 'yellow'];
 const hexColors = ['#CCCCFF', '#E97451', '#B2BEB5', '#DFFF00', '#FFB6C1', '#D2042D', '#F9F6EE'];
 
-const randomKeyColors = Math.floor(Math.random() * keywordColors.length);
-console.log(randomKeyColors, months[keywordColors]);
 
-const randomHexColors = Math.floor(Math.random() * hexColors.length);
-console.log(randomHexColors, months[hexColors]);
+const randomKeyColors = keywordColors[Math.floor(Math.random() * keywordColors.length)];
+
+const randomHexColors = hexColors[Math.floor(Math.random() * hexColors.length)];
 
 // Triangle tests
 describe('Triangle', () => {
-  const triangleKey = new Triangle('triangle', 'randomKeyColors');
+  const triangleKey = new Triangle('triangle', randomKeyColors);
 
   test('renders a triangle SVG element with keyword color', () => {
     expect(triangleKey.render()).toBe(
-      `<polygon points="150, 0 300, 200 0, 200" fill="red" />`
-      
+      `<polygon points='150, 0 300, 200 0, 200' fill='${randomKeyColors}' />`
       );
   });
 
-  const triangleHex = new Triangle('triangle', 'randomHexColors');
+  const triangleHex = new Triangle('triangle', randomHexColors);
 
   test('renders a triangle SVG element with hexidecimal color', () => {
-    expect(triangle.render()).toBe('<polygon points="150, 0 300, 200 0, 200" fill="red" />');
+    expect(triangleHex.render()).toBe(
+      `<polygon points='150, 0 300, 200 0, 200' fill='${randomHexColors}' />`
+      );
   });
 });
 
 // Circle tests
 describe('Circle', () => {
-  const circle = new Circle('circle', 'blue');
+  const circleKey = new Circle('circle', randomKeyColors);
 
-  test('has type property', () => {
-    expect(circle.type).toBe('circle');
+  test('renders a circle SVG element with keyword color', () => {
+    expect(circleKey.render()).toBe(
+      `<circle cx="150" cy="100" r="100" fill='${randomKeyColors}' />`
+      );
   });
 
-  test('has color property', () => {
-    expect(circle.color).toBe('blue');
-  });
+  const circleHex = new Circle('triangle', randomHexColors);
 
-  test('renders a circle SVG element', () => {
-    expect(circle.render()).toBe('<circle cx="150" cy="100" r="100" fill="blue" />');
+  test('renders a circle SVG element with hexidecimal color', () => {
+    expect(circleHex.render()).toBe(
+      `<circle cx="150" cy="100" r="100" fill='${randomHexColors}' />`
+      );
   });
 });
 
 // Square tests
 describe('Square', () => {
-  const square = new Square('square', 'green');
+  const squareKey = new Square('square', randomKeyColors);
 
-  test('has type property', () => {
-    expect(square.type).toBe('square');
+  test('renders a square SVG element with keyword color', () => {
+    expect(squareKey.render()).toBe(
+      `<rect width="100%" height="100%" fill='${randomKeyColors}' />`
+      );
   });
 
-  test('has color property', () => {
-    expect(square.color).toBe('green');
-  });
+  const squareHex = new Square('square', randomHexColors);
 
-  test('renders a square SVG element', () => {
-    expect(square.render()).toBe('<rect width="100%" height="100%" fill="green" />');
+  test('renders a square SVG element with hexidecimal color', () => {
+    expect(squareHex.render()).toBe(
+      `<rect width="100%" height="100%" fill='${randomHexColors}' />`
+      );
   });
 });
 
+// Text tests
+describe('Text', () => {
+  const textLetters = ['ASH', 'PRG', 'WEV', 'QRR', 'MBY', 'POL', 'CVR'];
+  const randomText = textLetters[Math.floor(Math.random() * textLetters.length)];
+
+  const textKey = new TextSVG(randomText, randomKeyColors);
+
+  test('renders a text SVG element with keyword color', () => {
+    expect(textKey.render()).toBe(
+      `<text x='50%' y='50%' font-size='60' text-anchor='middle' alignment-baseline='middle' fill='${randomKeyColors}'>${randomText}</text>`
+      );
+  });
+
+  const textHex = new TextSVG(randomText, randomHexColors);
+
+  test('renders a text SVG element with hexidecimal color', () => {
+    expect(textHex.render()).toBe(
+      `<text x='50%' y='50%' font-size='60' text-anchor='middle' alignment-baseline='middle' fill='${randomHexColors}'>${randomText}</text>`
+      );
+  });
+});
 
 
 
